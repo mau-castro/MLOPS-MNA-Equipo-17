4c3e65e90b30896cd3e6b71c544eb8a2b43556d8
Juan Carlos Romero <157056343+A00785001@users.noreply.github.com>
2024-10-13 21:19:22 -0600

    Update brain_tumor_refactored_v1.py

--- src/brain_tumor_refactored_v1.py
+++ src/brain_tumor_refactored_v1.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
